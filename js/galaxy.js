3576bb0578b6fd294f645b8271c32e56dd30385f
nighthowler-jj <2024jatinjee@gmail.com>
2026-02-07 03:14:46 +0530

    Format import statements in galaxy.js

--- js/galaxy.js
+++ js/galaxy.js
@@ -1,3 +1,8 @@
-import { Renderer, Program, Mesh, Color, Triangle } from 'https://unpkg.com/ogl@1.0.11/dist/ogl.mjs';
+import {
+  Renderer,
+  Program,
+  Mesh,
+  Color,
+  Triangle
+} from "https://unpkg.com/ogl@1.0.11/dist/ogl.mjs";
 
-initGalaxy();
